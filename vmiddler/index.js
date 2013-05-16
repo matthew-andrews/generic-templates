62fb16c37f8bbbbcd3f33d58de76c740fc7d3776
Matthew Andrews <matthew@andrews.eu.com>
2013-05-16 23:38:08 +0100

    Add a fruit to handle the vertical centering of any subfruit

---
+++ vmiddler/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * Vertical Middler Object
+ *
+ * @author Matt Andrews <matt@mattandre.ws>
+ * @copyright The Financial Times Limited
+ */
+
+module.exports = {
+  module: 'layout-a',
+  template: require('./template')
+};
